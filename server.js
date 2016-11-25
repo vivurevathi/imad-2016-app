a6ffc59c2265c13a96726fda21d2bb3df41fd221
vivurevathi <vivurevathi@gmail.com>
2016-11-25 11:46:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,17 +17,10 @@ app.get('/ui/vivu.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'vivu.jpg'));
 });
 
-app.get('/ui/abc.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'abc.jpg'));
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/ui/cd.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'cd.jpg'));
-});
-
-app.get('/ui/cv.JPG', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'cv.jpg'));
-});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
