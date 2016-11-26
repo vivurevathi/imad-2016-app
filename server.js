b814ad531c89d51763baa736cbe2311ae6a5bd28
vivurevathi <vivurevathi@gmail.com>
2016-11-26 21:48:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,47 +13,6 @@ app.get('/ui/main.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.css'));
 });
 
-app.get('/ui/preset1.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'preset1.css'));
-});
-
-app.get('/ui/preset2.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'preset2.css'));
-});
-
-app.get('/ui/preset3.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'preset3.css'));
-});
-
-app.get('/ui/preset4.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'preset4.css'));
-});
-
-app.get('/ui/preset5.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'preset5.css'));
-});
-
-app.get('/ui/preset6.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'preset6.css'));
-});
-
-app.get('/ui/lightbox.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'lightbox.css'));
-});
-app.get('/ui/animate.min.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'animate.min.css'));
-});
-app.get('/ui/responsive.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'responsive.css'));
-});
-
-app.get('/ui/bootstrap.min.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'bootstrap.min.css'));
-});
-
-app.get('/ui/font-awesome.min.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'font-awesome.min.css'));
-});
 
 app.get('/ui/1.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', '1.jpg'));
