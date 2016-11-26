00e72b5beed7ceccccb30404baa538588e6f5979
vivurevathi <vivurevathi@gmail.com>
2016-11-26 21:09:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,6 +71,22 @@ app.get('/ui/3.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', '3.jpg'));
 });
 
+app.get('/ui/4.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', '4.jpg'));
+});
+
+app.get('/ui/5.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', '5.jpg'));
+});
+
+app.get('/ui/6.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', '6.jpg'));
+});
+
+app.get('/ui/7.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', '7.jpg'));
+});
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
